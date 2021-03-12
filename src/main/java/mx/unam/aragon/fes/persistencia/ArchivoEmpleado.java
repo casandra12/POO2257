4e804503d5e401f9da278ed5f1c4693a2c84a557
LESRLUEN <cassmonse201@gmail.com>
2021-03-12 17:41:40 -0600

    12 de marzo del 2021

---
+++ src/main/java/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.aragon.fes.persistencia;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import mx.unam.aragon.fes.Empleado;
+
+/**
+ *
+ * @author Alan
+ */
+public class ArchivoEmpleado {
+    private String archivo;
+
+    public ArchivoEmpleado() {
+    }
+
+    public ArchivoEmpleado(String archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    
+     public void guardarEmpleados(ArrayList<Empleado> datos ){
+        // aqui el codigo para grabar ed Disco Duro
+        try {
+            ObjectOutputStream fSalida = new ObjectOutputStream(
+                    new FileOutputStream(archivo));
+
+            fSalida.writeObject(datos);
+            fSalida.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+public ArrayList<Empleado> leerEmpleados(){
+        ArrayList<Empleado> tmp = null;
+         try {
+             ObjectInputStream fLectura = new ObjectInputStream(
+                    new FileInputStream(archivo));
+
+            tmp = (ArrayList<Empleado>) fLectura.readObject();
+            if (tmp == null) {
+                System.out.println("No hay nada");
+            } else {
+                System.out.println("Nombre" + tmp.size());
+            }
+            fLectura.close();
+
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.toString());
+        }
+        
+        // Leer del disco duro
+        // se sube al Arraylist tmp
+        return tmp;
+    }
+}
